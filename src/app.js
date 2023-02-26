a763f57286405940474e560198589b8ea4fe1ff8
SalmaFiona1 <113827960+SalmaFiona1@users.noreply.github.com>
2023-02-26 12:58:55 +0000

    Update function search - submit

--- src/app.js
+++ src/app.js
@@ -1,3 +1,12 @@
 let apiKey = "03fbf04a1etcf05607fe0offcb23d041";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Lisbon&key=${apiKey}&units=metric`;
 console.log(apiUrl);
+
+function submitCity(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#user-city");
+  console.log(cityInput.value);
+}
+
+let form = document.querySelector("#search-city");
+form.addEventListener("submit", submitCity);
