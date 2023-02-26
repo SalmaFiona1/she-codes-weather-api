ddffc1255f3f7ae9294bc43abcb78779c7c01db5
SalmaFiona1 <113827960+SalmaFiona1@users.noreply.github.com>
2023-02-26 12:38:09 +0000

    Update units - metric

--- src/app.js
+++ src/app.js
@@ -1,2 +1,3 @@
 let apiKey = "03fbf04a1etcf05607fe0offcb23d041";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Lisbon&key=${apiKey}`;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Lisbon&key=${apiKey}&units=metric`;
+console.log(apiUrl);
